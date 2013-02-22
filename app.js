67e2aa92aaee55ac88d688747414540ea019199e
mochipful <apattabi.cmu@gmail.com>
2013-02-22 09:37:59 -0500

    Basic entry manipulation added.

--- app.js
+++ app.js
@@ -128,17 +128,34 @@ app.post("/answer", function(request, response) {
 var g_notebookList = [];
 
 function Notebook(name){
+	
+	// The name of the notebook to be added
 	this.name = name;
+	
+	// A list of all the available tags in a given notebook
 	this.alltags = [];
+	
+	// A list of all the entries in a given notebook
 	this.entries = [];
+	
+	// An object where the keys are the tag names, 
+	// The values are an array of indices into entries[] which have the tag that was used as the key.
+	this.access_database = {};
 }
 
-function Entry(){
+function Entry(entry){
 	this.content = "";
 	this.desc = "";
 	this.tags = [];
 	this.dateAdded = "";
 	this.dateAccessed = "";
+	this.deleted = false;
+	
+	if(entry.content){ this.content = entry.content}
+	if(entry.desc){ this.desc = entry.desc}
+	if(entry.tags){ this.tags = entry.tags}
+	if(entry.dateAdded){ this.dateAdded = entry.dateAdded}
+	if(entry.dateAccessed){ this.dateAccessed = entry.dateAccessed}	
 }
 
 function initServer() {
@@ -149,17 +166,35 @@ function initServer() {
   });
 }
 
+
+// ----------------------------------------------------------------------
+// All the functions for managing the Notebooks
+
 function initNotebook(name) {
 	//Create notebook object
 	var notebook = new Notebook(name);
 
 	//Create file for notebook object
-	writeFile(getDBFilename(name), JSON.stringify(notebook));
-
+	writeNotebookToFile(notebook);
+	
 	//Add to appropriate places
 	addToNotebookList(name);
 
 	return notebook;
+}
+
+// Extracts the notebook header to send back to the client.
+function getNotebookHeader(notebook){
+    return	{"notebook_header": {	"name": notebook.name,
+									"alltags": notebook.alltags,
+									"numEntries" : entries.length
+					            },
+					 "success": true}
+}
+
+// Persists a notebooks contents to file
+function writeNotebookToFile(notebook){
+	writeFile(getDBFilename(notebook.name), JSON.stringify(notebook));
 }
 
 // Updates the database and the global notebooks object with the information.
@@ -207,6 +242,19 @@ function getDBFilename(name){
 	return "database/" + name + ".txt";
 }
 
+// ----------------------------------------------------------------------
+// All the functions for managing the notebook entries
+
+// Checks whether a notebook entry is valid
+function validNotebookEntry(entry){
+	return /*(entry.tags !== [] && dateAccessed !==)*/ true;
+}
+
+
+// ----------------------------------------------------------------------
+// Server writes and responses
+
+
 // This is for serving files in the static directory
 app.get("/static/:staticFilename", function (request, response) {
     response.sendfile("static/" + request.params.staticFilename);
@@ -239,63 +287,139 @@ app.post('/create', function (request, response) {
 	}
 });
 
-/*
-// Deletes an existing notebook
-app.post('/delete', function (request, response) {
+// Updates a date in a notebook
+app.post('/upDate', function (request, response) {
 	var name = 	request.body.name;
+	var index = request.body.entryIndex;
+	var dateAccessed = request.body.dateAccessed;
 	
-	// Checks if the notebook name already exists and if its well-formed
-	if(!validNotebookName(name) || existingNotebookName(name)){
+	if(!existingNotebookName(name)){
+		console.log("Removes entry from non-existent entry.");
 		response.send({"success": false});
 	}
 	else{
-		var notebook = initNotebook(name);
-		response.send({"notebook": notebook,
-					   "success": true
+		readFile(getDBFilename(name), {}, function(err, data) {
+			notebook = JSON.parse(data);
+			console.log(notebook);
+			
+			if(index >= notebook.entries.length){
+				response.send({"success": false});
+			}
+						
+			notebook.entries[index].dateAccessed = dateAccessed;			
+			// Persist changes to notebook
+			writeNotebookToFile(notebook);
+			
+			// Send back the updated notebook
+			response.send({"notebook": notebook,
+						   "success": true
 					  });
-	}
+		});
+	}	
 });
 
-function validNotebookEntry(entry){
-	var notebook = ;
-}
+// Removes an entry from a notebook
+app.post('/removeEntry', function (request, response) {
+	var name = 	request.body.name;
+	var index = request.body.entryIndex;
+	
+	// Checks if the notebook name already exists and if its well-formed
+	if(!existingNotebookName(name)){
+		console.log("Removes entry from non-existent entry.");
+		response.send({"success": false});
+	}
+	else{
+		readFile(getDBFilename(name), {}, function(err, data) {
+			notebook = JSON.parse(data);
+			console.log(notebook);
+			
+			if(index >= notebook.entries.length){
+				response.send({"success": false});
+			}
+						
+			notebook.entries[index].deleted = true;			
+			
+			// Persist changes to notebook
+			writeNotebookToFile(notebook);
+			
+			// Send back the updated notebook
+			response.send({"notebook": notebook,
+						   "success": true
+					  });
+		});
+	}	
+});
 
-function addEntry(name, entry){
-	this.content = "";
-	this.desc = "";
-	this.tags = [];
-	this.dateAccessed = "";
-}
 
 // Adds a new entry to an existing notebook
 app.post('/addEntry', function (request, response) {
 	var name = 	request.body.name;
-	var entry = JSON.parse(request.body.entry);
+	var entry = request.body.entry;
 	
 	// Checks if the notebook name already exists and if its well-formed
-	if(!validNotebookName(name) || existingNotebookName(name) || !validNotebookEntry(entry)){
+	if(!validNotebookName(name) || !existingNotebookName(name) || !validNotebookEntry(entry)){
+		console.log("Something went wrong");
 		response.send({"success": false});
 	}
 	else{
-		var notebook = addEntry(name, entry);
-		response.send({"notebook": notebook,
-					   "entry": entry,
-					   "success": true
+		readFile(getDBFilename(name), {}, function(err, data) {
+			notebook = JSON.parse(data);
+			console.log(notebook);
+			var dbEntry = new Entry(entry);
+			
+			// Add entry to list of entries in notebook, get its index
+			var index = notebook.entries.push(dbEntry) - 1;
+			
+			// Add any tags to master list of tags
+			// Add to access_database
+			for(var i = 0; i < dbEntry.tags.length; i++){
+				var tag = dbEntry.tags[i];
+				if(notebook.alltags.indexOf(tag) < 0){
+					notebook.alltags.push(tag);
+					notebook.access_database[tag] = [index];
+				}
+				else{
+					notebook.access_database[tag].push(index);
+				}
+			}
+			
+			// Persist changes to notebook
+			writeNotebookToFile(notebook);
+			
+			// Send back the formatted databaseEntry
+			response.send({"notebook": notebook,
+							"entry" : dbEntry,
+						   "success": true
 					  });
+		});
 	}	
 });
 
-*/
-
-
-// Loads an existing notebook
+// Loads the list of notebooks for someone to look at.
 app.get('/notebooks', function (request, response) {
 	response.send({"list": g_notebookList,
 				   "success": true
 	});
 });
 
+// Loads an existing notebook header
+app.get('/loadHeader/:name', function (request, response) {
+	var name = request.params.name;
+	var notebook;
 
+	if(existingNotebookName(name)){
+		readFile(getDBFilename(name), {}, function(err, data) {
+			notebook = JSON.parse(data);
+			console.log(notebook);
+			
+			// Send back the header for the notebook, but not the entries
+			response.send(getNotebookHeader(notebook));
+		});
+	}
+	else{
+		response.send({"success": false});
+	}
+});
 
 // Loads an existing notebook
 app.get('/load/:name', function (request, response) {
@@ -306,14 +430,127 @@ app.get('/load/:name', function (request, response) {
 		readFile(getDBFilename(name), {}, function(err, data) {
 			notebook = JSON.parse(data);
 			console.log(notebook);
-			response.send({"notebook": notebook,
-							"success": true});
+			
+			// Send back the header for the notebook, but not the entries
+			//response.send(getNotebookHeader(notebook));
+			response.send({"notebook_header": notebook, "success": true});
 		});
 	}
 	else{
 		response.send({"success": false});
 	}
 });
+
+// Searches an existing notebook for a tag
+app.get('/search/:name/:tag', function (request, response) {
+	var name = request.params.name;
+	var tag = request.params.tag;
+
+	if(existingNotebookName(name)){
+		readFile(getDBFilename(name), {}, function(err, data) {
+			var notebook = JSON.parse(data);
+			console.log(notebook);
+			
+			var searchResults = notebook.access_database[tag];
+			
+			if(!searchResults){
+				response.send({"found": false, "success": true});				
+			}
+			
+			var returnEntries = [];
+			var newSearchResults = [];
+			var writeToFile = false;
+			
+			// As we search. condense the database with deleted entries.
+			for(var i = 0; i < searchResults.length; i++){
+				var entry = notebook.entries[searchResults[i]];
+				if(!entry.deleted){
+					returnEntries.push(entry);
+					newSearchResults.push(searchResults[i]);
+					writeToFile = true;
+				}
+			}
+			
+			notebook.access_database[tag] = newSearchResults;
+			
+			// If the notebook has been condensed by the results
+			if(writeToFile){
+				writeNotebookToFile(notebook);
+			}
+			
+			// Send back the header for the notebook, but not the entries
+			//response.send(getNotebookHeader(notebook));
+			response.send({"results": returnEntries, "found": true, "success": true});
+		});
+	}
+	else{
+		response.send({"found": false, "success": false});
+	}
+});
+
+// ---------------------------------------------------------------------
+// Some basic checks to make sure our database stays sanitary
+
+function generateAccessDatabase(notebook){
+	var new_access = {};
+	var new_alltags = [];
+	for(j = 0; j < notebook.entries.length; j++){
+		var entry = notebook.entries[j];
+
+		if(!entry.deleted){
+			// Add any tags to master list of tags
+			// Add to access_database
+			for(var i = 0; i < entry.tags.length; i++){
+				var tag = entry.tags[i];
+				if(new_alltags.indexOf(tag) < 0){
+					new_alltags.push(tag);
+					new_access[tag] = [j];
+				}
+				else{
+					new_access[tag].push(j);
+				}
+			}		
+		}
+	}
+	
+	notebook.alltags = new_alltags;
+	notebook.access_database = new_access;
+}
+
+/*
+function checkNotebook(name){
+	if(name === "notebooks"){ return false; }
+	
+	// Check that each tag in the access database is present in all tags
+	// Check that 
+	readFile(getDBFilename(name), {}, function(err, data) {
+		var notebook = JSON.parse(data);
+		
+		for(var j = 0; j < notebook.entries.length; j++){
+			var entry = notebook.entries[j];
+			
+			if(!entry.deleted){
+				// Add any tags to master list of tags
+				// Add to access_database
+				for(var i = 0; i < entry.tags.length; i++){
+					var tag = entry.tags[i];
+					// If tag isn't present in all tags
+					if(notebook.alltags.indexOf(tag) < 0){
+						console.log( "Tag not found in all tags.");
+						return false;
+					}
+					// If tag isn't present in proper place in access
+					if(notebook.access_database[tag].indexOf(j) < 0){
+						console.log("Tag not found in access database");
+						return false;
+					}
+				}
+			}
+		}
+	}
+}*/
+
+
 
 // Finally, initialize the server, then activate the server at port 8889
 initServer();
--- static/notebook.js
+++ static/notebook.js
@@ -155,6 +155,7 @@
 
 //----------------------------------------
 var g_notebook;
+var g_searchResults;
 
   // Adds a notebook to the database
   function addNotebook(name) {
@@ -168,6 +169,18 @@ var g_notebook;
     });
   }
 
+
+  // Gets a notebook header from the database
+  function getNotebookHeader(name) {
+    $.ajax({
+      type: "get",
+      url: "/loadHeader/" + name,
+      success: function(data) {
+        g_notebook = data.notebook_header;
+		//console.log(g_notebook);
+	  }
+    });
+  }  
   
   // Gets a notebook from the database
   function getNotebook(name) {
@@ -175,12 +188,73 @@ var g_notebook;
       type: "get",
       url: "/load/" + name,
       success: function(data) {
-        g_notebook = data.notebook;
+        g_notebook = data.notebook_header;
 		//console.log(g_notebook);
 	  }
     });
   }
 
+  // Add entry with given content, tag name
+  function addEntryWithData(name, content, listOfTags){
+	date = new Date();
+	if(!name || !listOfTags){
+	}
+	else{
+		addEntry(name, {"content": content, "tags": listOfTags, 
+			"dateAccessed": date, "dateAdded": date});
+	}
+  }
+  
+  // Adds a notebook to the database
+  function addEntry(name, entry) {
+    $.ajax({
+      type: "post",
+      data: {"name": name, "entry": entry},
+      url: "/addEntry",
+      success: function(data) {
+		console.log(data.notebook);
+        //g_notebook = data.notebook;
+      }
+    });
+  }
+
+  // Updates the dateAccessed
+  function upDate(name, entryIndex, dateAccessed) {
+    $.ajax({
+      type: "post",
+      data: {"name": name, "entryIndex": entryIndex, 
+			 "dateAccessed": dateAccessed},
+      url: "/upDate",
+      success: function(data) {
+        //g_notebook = data.notebook;
+      }
+    });
+  }  
+
+  // Remove an entry
+  function removeEntry(name, entryIndex) {
+    $.ajax({
+      type: "post",
+      data: {"name": name, "entryIndex": entryIndex},
+      url: "/removeEntry",
+      success: function(data) {
+        //g_notebook = data.notebook;
+      }
+    });
+  }    
+  
+  // Searches a notebook for a tag
+  function searchNotebook(name, tag) {
+    $.ajax({
+      type: "get",
+      url: "/search/" + name + "/" + tag,
+      success: function(data) {
+		g_searchResults = data.results;
+	  }
+    });
+  }
+
+  
   // Logs the list of notebooks from the database.
   // This is supposed to be a secret function.
   function getNotebooks() {
@@ -193,9 +267,41 @@ var g_notebook;
     });
   }
 
+
+function checkNotebook(notebook){
+	if(notebook.name === "notebooks"){ 
+		console.log("checkNotebook: notebook name can't be notebooks.");
+		return false; 
+	}
+		
+	for(var j = 0; j < notebook.entries.length; j++){
+		var entry = notebook.entries[j];
+		
+		if(!entry.deleted){
+			// Add any tags to master list of tags
+			// Add to access_database
+			for(var i = 0; i < entry.tags.length; i++){
+				var tag = entry.tags[i];
+				// If tag isn't present in all tags
+				if(notebook.alltags.indexOf(tag) < 0){
+					console.log( "checkNotebook: Tag not found in all tags.");
+					return false;
+				}
+				// If tag isn't present in proper place in access
+				if(notebook.access_database[tag].indexOf(j) < 0){
+					console.log("checkNotebook: Tag not found in access database");
+					return false;
+				}
+			}
+		}
+	}
+	
+	console.log("checkNotebook: success");
+}    
+  
   $(document).ready(function() {
 	    console.log("Some functions: ");
-	    console.log("getNotebooks(), addNotebook(name), getNotebook(name)");
+	    console.log("getNotebooks(), addNotebook(name), getNotebook(name), addEntryWithData(name, content, listOfTags), removeEntry(name, entryIndex), upDate(name, entryIndex, dateAccessed), checkNotebook(notebook), searchNotebook(name, tag)");
       var urlSegments = window.location.pathname.split('/');
       urlSegments.splice(0,1); // removes "" from the beginning of list
       if(urlSegments[0] === "notebook" && urlSegments.length > 1){
