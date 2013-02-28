ba6c5d238960bea4c0a171b818227cb58c7b428e
Dan Yang <dsyang92@gmail.com>
2013-02-27 21:28:14 -0500

    reordered app.js and default ordering

--- app.js
+++ app.js
@@ -76,19 +76,6 @@ function Entry(entry, index){
 	if(index !== undefined){ this.index = index}
 }
 
-function initServer() {
-  // When we start the server, we must load the stored data
-  var defaultList = "[]";
-  readFile(getDBFilename("notebooks"), defaultList, function(err, data) {
-	g_notebookList = JSON.parse(data);
-	checkAllNotebooks();
-  });
-  //get list of static files
-  fs.readdir("static/", function(err, files) {
-      g_staticFiles = files;
-  });
-}
-
 
 // ----------------------------------------------------------------------
 // All the functions for managing the Notebooks
@@ -205,6 +192,238 @@ function validNotebookEntry(entry){
 }
 
 
+function addEntryTags(notebook, dbEntry){
+	var index = dbEntry.index;
+
+	// Add any tags to master list of tags
+	// Add to access_database
+	for(var i = 0; i < dbEntry.tags.length; i++){
+		var tag = dbEntry.tags[i];
+		if(notebook.access_database[tag] === undefined){
+			notebook.access_database[tag] = [index];
+		}
+		else{
+			notebook.access_database[tag].push(index);
+		}
+	}
+}
+
+function deleteEntryTags(notebook, index){
+	var oldEntry = notebook.entries[index];
+
+	// Removes all of the oldEntry tags.
+	for(var i = 0; i < oldEntry.tags.length; i++){
+		var tag = oldEntry.tags[i];
+		var accessDBList = notebook.access_database[tag];
+
+		console.log("types: " + typeof(index) + typeof(accessDBList[0]))
+		console.log("Removing Index: " + index);
+		console.log("Located at: " + accessDBList.indexOf(index));
+		console.log(accessDBList);
+		// use this (removes the index from tag list)
+		accessDBList.splice(accessDBList.indexOf(index), 1);
+		console.log(accessDBList);
+
+		// Remove this tag from the database if the database is empty.
+		if(accessDBList.length === 0){
+			delete notebook.access_database[tag];
+		}
+	}
+}
+
+
+
+/*2.2. Reserved Characters
+
+Many URI include components consisting of or delimited by, certain special
+characters. These characters are called "reserved", since their usage within the
+URI component is limited to their reserved purpose. If the data for a URI
+component would conflict with the reserved purpose, then the conflicting data
+must be escaped before forming the URI.
+
+ reserved    = ";" | "/" | "?" | ":" | "@" | "&" | "=" | "+" |
+                "$" | ","
+The "reserved" syntax class above refers to those characters that are allowed
+ within a URI, but which may not be allowed within a particular component of
+ the generic URI syntax
+
+*/
+
+// Parse tokens
+function getParseTokens(){
+	return {
+		'union' : "+",
+		'intersection' : "^^"
+	};
+}
+
+// Fetch the union of all the tags
+function unionTags(notebook, tags){
+	var list = [];
+	for(var i = 0; i < tags.length; i++){
+		var tag = tags[i];
+
+		var tagList = notebook.access_database[tag];
+		if(tagList !== undefined){
+			list = list.concat(tagList);
+		}
+	}
+	list = list.filter(function(elem, ind){
+		return list.indexOf(elem) === ind;
+	})
+	return list;
+}
+
+// Fetch the intersect of all the tags
+function intersectTags(notebook, tags){
+	var count = {};
+	var list =[];
+	for(var i = 0; i < tags.length; i++){
+		var tag = tags[i];
+
+		var tagList = notebook.access_database[tag];
+
+		// Check whether the tag has anything yet.
+		if(tagList !== undefined){
+			tagList.forEach(
+			function(elem){
+				var index = elem + "";
+				var curr = count[index];
+				if(curr === undefined){
+					count[index] = 1;
+				}
+				else{
+					count[index] = curr + 1;
+				}
+			})
+		}
+	}
+
+	for(prop in count){
+		if(count[prop] === tags.length){
+			list.push(parseInt(prop));
+		}
+	}
+	return list;
+}
+
+// Parses a tag search request and returns the list of entries.
+function parseSearch(notebook, tagString){
+	var tokens = getParseTokens();
+	var tags = [];
+	var type;
+	var searchResults;
+
+	if(tagString.indexOf(tokens.union) >= 0){
+		tags = tagString.split(tokens.union);
+		searchResults = unionTags(notebook, tags);
+	}
+	else if(tagString.indexOf(tokens.intersection) >= 0){
+		tags = tagString.split(tokens.intersection);
+		searchResults = intersectTags(notebook, tags);
+	}
+	else{
+		searchResults = notebook.access_database[tagString];
+	}
+
+	// This is to denote a malformed request.
+	if(searchResults === undefined){
+		return undefined;
+	}
+
+	var returnEntries = [];
+
+	// Actually fetch the entries from the determined indices.
+	for(var i = 0; i < searchResults.length; i++){
+		returnEntries.push(notebook.entries[searchResults[i]]);
+	}
+
+	return returnEntries;
+}
+
+
+// ---------------------------------------------------------------------
+// Some basic checks to make sure our database stays sanitary
+
+function checkAllNotebooks(){
+	var notebookList = g_notebookList;
+
+	for(var i = 0; i < notebookList.length; i++){
+		var name = notebookList[i];
+		readFile(getDBFilename(name), {}, function(err, data) {
+			var notebook = JSON.parse(data);
+
+			if(!checkAccessDatabase(notebook)){
+			}
+		});
+
+		console.log("Done checking notebooks.");
+	}
+}
+
+function checkAccessDatabase(notebook){
+	var entries = notebook.entries;
+	for(var prop in notebook.access_database){
+		var arr = notebook.access_database[prop];
+		for(var i = 0; i < arr.length; i++){
+			if(entries[arr[i]].deleted === true){
+				console.log("Malformed notebook:" + notebook.name);
+				console.log("Property: " +  prop + " : " + i);
+				printEntrySummary(notebook.entries);
+				generateAccessDatabase(notebook);
+				writeNotebookToFile(notebook);
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+function generateAccessDatabase(notebook){
+	var new_access = {};
+	for(j = 0; j < notebook.entries.length; j++){
+		var entry = notebook.entries[j];
+
+		if(!entry.deleted){
+			// Add any tags to master list of tags
+			// Add to access_database
+			for(var i = 0; i < entry.tags.length; i++){
+				var tag = entry.tags[i];
+				if(new_access[tag] === undefined){
+					new_access[tag] = [j];
+				}
+				else{
+					new_access[tag].push(j);
+				}
+			}
+		}
+	}
+	printAccessDatabase(notebook.access_database);
+	printAccessDatabase(new_access);
+
+	notebook.access_database = new_access;
+}
+
+function printEntrySummary(entries){
+	console.log("Entries: ");
+	for(var i = 0; i < entries.length; i++){
+		var entry = entries[i];
+		console.log(i + " : " + entry.deleted + " : " + "tags: " + entry.tags);
+	}
+}
+
+function printAccessDatabase(accessDB){
+	console.log("Access")
+	for(var prop in accessDB){
+		var arr = accessDB[prop];
+		console.log(prop + " : " + arr);
+	}
+	return true;
+}
+
+//--------------------------------------------------------------
+// Express routes
+
 // ----------------------------------------------------------------------
 // Server writes and responses
 
@@ -223,6 +442,8 @@ app.get("/notebook/:name", function (request, response) {
         response.sendfile("static/" + request.params.name);
     }
 });
+
+
 
 // Creates a new notebook
 app.post('/create', function (request, response) {
@@ -273,45 +494,6 @@ app.post('/upDate', function (request, response) {
 		});
 	}
 });
-
-function addEntryTags(notebook, dbEntry){
-	var index = dbEntry.index;
-
-	// Add any tags to master list of tags
-	// Add to access_database
-	for(var i = 0; i < dbEntry.tags.length; i++){
-		var tag = dbEntry.tags[i];
-		if(notebook.access_database[tag] === undefined){
-			notebook.access_database[tag] = [index];
-		}
-		else{
-			notebook.access_database[tag].push(index);
-		}
-	}
-}
-
-function deleteEntryTags(notebook, index){
-	var oldEntry = notebook.entries[index];
-
-	// Removes all of the oldEntry tags.
-	for(var i = 0; i < oldEntry.tags.length; i++){
-		var tag = oldEntry.tags[i];
-		var accessDBList = notebook.access_database[tag];
-
-		console.log("types: " + typeof(index) + typeof(accessDBList[0]))
-		console.log("Removing Index: " + index);
-		console.log("Located at: " + accessDBList.indexOf(index));
-		console.log(accessDBList);
-		// use this (removes the index from tag list)
-		accessDBList.splice(accessDBList.indexOf(index), 1);
-		console.log(accessDBList);
-
-		// Remove this tag from the database if the database is empty.
-		if(accessDBList.length === 0){
-			delete notebook.access_database[tag];
-		}
-	}
-}
 
 // Removes an entry from a notebook
 app.post('/removeEntry', function (request, response) {
@@ -501,114 +683,6 @@ app.get('/loadEntries/:name', function (request, response) {
 	}
 });
 
-/*2.2. Reserved Characters
-
-Many URI include components consisting of or delimited by, certain special
-characters. These characters are called "reserved", since their usage within the
-URI component is limited to their reserved purpose. If the data for a URI
-component would conflict with the reserved purpose, then the conflicting data
-must be escaped before forming the URI.
-
- reserved    = ";" | "/" | "?" | ":" | "@" | "&" | "=" | "+" |
-                "$" | ","
-The "reserved" syntax class above refers to those characters that are allowed
- within a URI, but which may not be allowed within a particular component of
- the generic URI syntax
-
-*/
-
-// Parse tokens
-function getParseTokens(){
-	return {
-		'union' : "+",
-		'intersection' : "^^"
-	};
-}
-
-// Fetch the union of all the tags
-function unionTags(notebook, tags){
-	var list = [];
-	for(var i = 0; i < tags.length; i++){
-		var tag = tags[i];
-
-		var tagList = notebook.access_database[tag];
-		if(tagList !== undefined){
-			list = list.concat(tagList);
-		}
-	}
-	list = list.filter(function(elem, ind){
-		return list.indexOf(elem) === ind;
-	})
-	return list;
-}
-
-// Fetch the intersect of all the tags
-function intersectTags(notebook, tags){
-	var count = {};
-	var list =[];
-	for(var i = 0; i < tags.length; i++){
-		var tag = tags[i];
-
-		var tagList = notebook.access_database[tag];
-
-		// Check whether the tag has anything yet.
-		if(tagList !== undefined){
-			tagList.forEach(
-			function(elem){
-				var index = elem + "";
-				var curr = count[index];
-				if(curr === undefined){
-					count[index] = 1;
-				}
-				else{
-					count[index] = curr + 1;
-				}
-			})
-		}
-	}
-
-	for(prop in count){
-		if(count[prop] === tags.length){
-			list.push(parseInt(prop));
-		}
-	}
-	return list;
-}
-
-// Parses a tag search request and returns the list of entries.
-function parseSearch(notebook, tagString){
-	var tokens = getParseTokens();
-	var tags = [];
-	var type;
-	var searchResults;
-
-	if(tagString.indexOf(tokens.union) >= 0){
-		tags = tagString.split(tokens.union);
-		searchResults = unionTags(notebook, tags);
-	}
-	else if(tagString.indexOf(tokens.intersection) >= 0){
-		tags = tagString.split(tokens.intersection);
-		searchResults = intersectTags(notebook, tags);
-	}
-	else{
-		searchResults = notebook.access_database[tagString];
-	}
-
-	// This is to denote a malformed request.
-	if(searchResults === undefined){
-		return undefined;
-	}
-
-	var returnEntries = [];
-
-	// Actually fetch the entries from the determined indices.
-	for(var i = 0; i < searchResults.length; i++){
-		returnEntries.push(notebook.entries[searchResults[i]]);
-	}
-
-	return returnEntries;
-}
-
 // Searches an existing notebook for a tag
 app.get('/search/:name/:tags', function (request, response) {
 	var name = request.params.name;
@@ -631,86 +705,25 @@ app.get('/search/:name/:tags', function (request, response) {
 	}
 });
 
-// ---------------------------------------------------------------------
-// Some basic checks to make sure our database stays sanitary
 
-function checkAllNotebooks(){
-	var notebookList = g_notebookList;
 
-	for(var i = 0; i < notebookList.length; i++){
-		var name = notebookList[i];
-		readFile(getDBFilename(name), {}, function(err, data) {
-			var notebook = JSON.parse(data);
+//--------------------------------------------------------------------- 
+//Finally, initialize the server, then activate the server at port 8889
 
-			if(!checkAccessDatabase(notebook)){
-			}
-		});
 
-		console.log("Done checking notebooks.");
-	}
+function initServer() {
+  // When we start the server, we must load the stored data
+  var defaultList = "[]";
+  readFile(getDBFilename("notebooks"), defaultList, function(err, data) {
+	g_notebookList = JSON.parse(data);
+	checkAllNotebooks();
+  });
+  //get list of static files
+  fs.readdir("static/", function(err, files) {
+      g_staticFiles = files;
+  });
 }
 
-function checkAccessDatabase(notebook){
-	var entries = notebook.entries;
-	for(var prop in notebook.access_database){
-		var arr = notebook.access_database[prop];
-		for(var i = 0; i < arr.length; i++){
-			if(entries[arr[i]].deleted === true){
-				console.log("Malformed notebook:" + notebook.name);
-				console.log("Property: " +  prop + " : " + i);
-				printEntrySummary(notebook.entries);
-				generateAccessDatabase(notebook);
-				writeNotebookToFile(notebook);
-				return false;
-			}
-		}
-	}
-	return true;
-}
-
-function generateAccessDatabase(notebook){
-	var new_access = {};
-	for(j = 0; j < notebook.entries.length; j++){
-		var entry = notebook.entries[j];
-
-		if(!entry.deleted){
-			// Add any tags to master list of tags
-			// Add to access_database
-			for(var i = 0; i < entry.tags.length; i++){
-				var tag = entry.tags[i];
-				if(new_access[tag] === undefined){
-					new_access[tag] = [j];
-				}
-				else{
-					new_access[tag].push(j);
-				}
-			}
-		}
-	}
-	printAccessDatabase(notebook.access_database);
-	printAccessDatabase(new_access);
-
-	notebook.access_database = new_access;
-}
-
-function printEntrySummary(entries){
-	console.log("Entries: ");
-	for(var i = 0; i < entries.length; i++){
-		var entry = entries[i];
-		console.log(i + " : " + entry.deleted + " : " + "tags: " + entry.tags);
-	}
-}
-
-function printAccessDatabase(accessDB){
-	console.log("Access")
-	for(var prop in accessDB){
-		var arr = accessDB[prop];
-		console.log(prop + " : " + arr);
-	}
-	return true;
-}
-
-// Finally, initialize the server, then activate the server at port 8889
 initServer();
 app.listen(8889);
 console.log("created server on port 8889");
